2b5b6b4b604b68d58c2d37b006b7a02eb81e106c
Orvyl Tumaneng <orvyl.tumaneng@voyagerinnovation.com>
2017-12-06 19:48:18 +0800

    add id generator for both numeric and anumeric type

--- playgroud.go
+++ playgroud.go
@@ -1,1 +1,14 @@
-package kit
+package main
+
+import "github.com/orvyl/kit/id"
+import "fmt"
+
+func main() {
+	idGen, err := id.NewGenerator(id.Settings{IsAlphaNumeric: true})
+  if err != nil {
+    fmt.Printf("ERR %v", err)
+  }
+  id, err := idGen.Next()
+
+	fmt.Printf("%v %v", id, err)
+}
